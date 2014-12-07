3a98bec7d73eb963b5cf4a17cb051311992f7229
Wighawag <wighawag@gmail.com>
2014-12-07 06:27:52 +0000

    fix and support comment

--- src/hxml/Main.hx
+++ src/hxml/Main.hx
@@ -35,33 +35,37 @@ class Main extends mcli.CommandLine{
     					break;
     				}
     				
+                    var commentIndex = line.indexOf("#");
+                    if(commentIndex != -1){
+                        line = line.substr(0,commentIndex);
+                    }
     				if (line.indexOf("-lib ") != -1){
     					var lib = line.replace("-lib ", "");
-					var split = lib.split(":");
-    				//split.unshift("install");
-    				var libName = split[0];
-    				var libVersion = "";
-    				if(split.length > 1){
-    					libVersion = split[1];
-    				}
+    					var splits = lib.split(":");
 
-    				var haxelibProcess = new Process("haxelib", ["install", libName, libVersion]);
-    				haxelibProcess.stdin.writeString("n\n"); // do not set if older version was choosen while the newest version was already installed
-					var output = haxelibProcess.stdout.readAll().toString();//wait
-					Sys.println(output);
+                        var args = ["install"];
+                        for (split in splits){
+                            args.push(split.trim());
+                        }
+        				    
 
-	    			var stderr = haxelibProcess.stderr.readAll().toString();
-	    			
-	    			if(stderr != ""){
-	    				Sys.println(stderr);
-	    				errored = true;
-	    			}
-	    			
-	    			var exitCode = haxelibProcess.exitCode();
-	    			if(exitCode != 0){
-	    				errored = true;
-	    				Sys.println("haxelib exited with exitCode : " + exitCode);
-	    			}    					
+        				var haxelibProcess = new Process("haxelib", args);
+        				haxelibProcess.stdin.writeString("n\n"); // do not set if older version was choosen while the newest version was already installed
+    					var output = haxelibProcess.stdout.readAll().toString();//wait
+    					Sys.println(output);
+
+    	    			var stderr = haxelibProcess.stderr.readAll().toString();
+    	    			
+    	    			if(stderr != ""){
+    	    				Sys.println(stderr);
+    	    				errored = true;
+    	    			}
+    	    			
+    	    			var exitCode = haxelibProcess.exitCode();
+    	    			if(exitCode != 0){
+    	    				errored = true;
+    	    				Sys.println("haxelib exited with exitCode : " + exitCode);
+    	    			}    					
     				}
     				
     			}
